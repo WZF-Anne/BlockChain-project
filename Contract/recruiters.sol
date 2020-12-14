ddbe9ab3ba06ad4e81928c1eae2f4b66783e4124
WZF-Anne <2975981518@qq.com>
2020-12-14 23:19:24 +0800

    project

--- Contract/recruiters.sol
+++ Contract/recruiters.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: WZF
+ // SPDX-License-Identifier: WZF
 pragma solidity ^0.7.0;
 //多方招聘方公司进行转移查询员工工作记录
 contract recruiter{
@@ -15,43 +15,62 @@ contract recruiter{
     }
     
     struct staff{
-        string username;
-        string password;
-        string selfvalue;
-        address staffAddress;
+          string username; //职员名称
+        string password; //密码
+        string selfvalue;//个人价值
+        string entryTime; //入职时间
+        string resignTime; //离职时间
+        string performance; //业绩
+        address staffAddress; //对应职员地址
     }
 
     mapping(uint32 => staff) public staffs;
     
-   function addStaff(
+function addStaff(
         string memory _name,
         string memory _pwd,
         string memory _value,
+        string memory _Entime,
+        string memory _retime,
+        string memory _performance,
         address _address
     ) public returns (uint32) {
+        
         uint32 staffId = staff_id++;
         staffs[staffId].username = _name;
         staffs[staffId].password = _pwd;
         staffs[staffId].selfvalue = _value;
+        staffs[staffId].entryTime =_Entime;
+        staffs[staffId].resignTime =_retime;
+        staffs[staffId].performance = _performance;
         staffs[staffId].staffAddress = _address;
-
+        
        return staffId;
     }
-    
- //查询
-    function getStaff(uint32 staff_id)
+    //查询
+    function getStaff(uint32 staffid)
         public
         view
         returns (
+            string memory,
+            string memory,
+            string memory,
             string memory,
             string memory,
             address
         )
     {
         return (
-            staffs[staff_id].username,
-             staffs[staff_id].selfvalue,
-             staffs[staff_id].staffAddress
+            staffs[staffid].username,
+             staffs[staffid].selfvalue,
+             staffs[staffid].entryTime,
+             staffs[staffid].resignTime,
+             staffs[staffid].performance,
+             staffs[staffid].staffAddress
         );
     }
+    
+    
 }
+   
+       
